25d5ee7eeda162076051e49a58f236dd245d7bb8
Ilia <ilia.iliev94@gmail.com>
2026-02-26 15:58:15 +0200

    fix for pipewire

--- scripts-common/.local/bin/audio-inhibit.sh
+++ scripts-common/.local/bin/audio-inhibit.sh
@@ -2,8 +2,7 @@
 # Checks for active audio every 30 seconds and resets X11 idle timer
 
 while true; do
-    # Check for active audio streams (works for both Pulse and PipeWire)
-    if pactl list sink-inputs | grep -q "State: RUNNING"; then
+    if pactl list sink-inputs | grep -q "Corked: no"; then
         xset s reset
     fi
     sleep 30
